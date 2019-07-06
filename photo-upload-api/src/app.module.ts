22329af154a8988816a84727276bbc38ec222689
luvargas <luvargas@greathealthworks.com>
2019-07-06 18:22:57 -0400

    Add README files

--- photo-upload-api/src/app.module.ts
+++ photo-upload-api/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PhotosController } from './controllers/photo-upload/photos.controller';
+import { PhotosController } from './controllers/photos/photos.controller';
 
 @Module({
   imports: [],
